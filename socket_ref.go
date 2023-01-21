d393491b56a382965fb2fb0e845d51f6bb311173
Michał Korman <michal.korman@icloud.com>
2023-01-21 13:12:35 +0100

    SocketReference -> SocketRef

--- socket_ref.go
+++ socket_ref.go
@@ -6,22 +6,22 @@ import (
 	"sync/atomic"
 )
 
-// SocketReference allows to hold a reference to a socket outside its designated handler.
+// SocketRef allows to hold a reference to a socket outside its designated handler.
 // tinytcp performs no memory allocations on critical paths, which means it needs to pool some objects.
 // These pooled objects are typically structures that exist only in the context of a specific connection (eg. Sockets).
 // Objects are returned to the pool as they are no longer needed and reused by other connection in the future.
 // The rule is that a socket instance is only valid inside its designated handler and storing it outside this handler
-// might result in some very nasty bugs. SocketReference provides a way to safely store a reference to a socket,
+// might result in some very nasty bugs. SocketRef provides a way to safely store a reference to a socket,
 // and provide a subset of its functionalities.
-type SocketReference struct {
+type SocketRef struct {
 	s        *Socket
 	m        sync.RWMutex
 	recycled uint32
 }
 
-// NewSocketHolder creates an instance of SocketReference.
-func NewSocketHolder(s *Socket) *SocketReference {
-	ref := &SocketReference{
+// NewSocketRef creates an instance of SocketReference.
+func NewSocketRef(s *Socket) *SocketRef {
+	ref := &SocketRef{
 		s: s,
 	}
 
@@ -30,20 +30,20 @@ func NewSocketHolder(s *Socket) *SocketReference {
 }
 
 // Write writes data to a socket only if it hasn't been recycled yet.
-func (s *SocketReference) Write(b []byte) (int, error) {
-	s.m.RLock()
-	defer s.m.RUnlock()
+func (r *SocketRef) Write(b []byte) (int, error) {
+	r.m.RLock()
+	defer r.m.RUnlock()
 
-	if atomic.LoadUint32(&s.recycled) == 1 {
+	if atomic.LoadUint32(&r.recycled) == 1 {
 		return 0, io.EOF
 	}
 
-	return s.s.Write(b)
+	return r.s.Write(b)
 }
 
-func (s *SocketReference) onRecycle() {
-	s.m.Lock()
-	defer s.m.Unlock()
+func (r *SocketRef) onRecycle() {
+	r.m.Lock()
+	defer r.m.Unlock()
 
-	atomic.StoreUint32(&s.recycled, 1)
+	atomic.StoreUint32(&r.recycled, 1)
 }
