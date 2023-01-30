4554fea55a867ae6ffec371887ea4ca1d396176c
Michał Korman <michal.korman@icloud.com>
2023-01-30 11:12:38 +0100

    add optional config for promtinytcp

--- promtinytcp/metrics.go
+++ promtinytcp/metrics.go
@@ -5,32 +5,61 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// Config specifies an optional config for NewHandler.
+type Config struct {
+	// Namespace is a parameter attached to all Prometheus metrics registered in NewHandler.
+	Namespace string
+
+	// Subsystem is a parameter attached to all Prometheus metrics registered in NewHandler.
+	Subsystem string
+}
+
 // NewHandler creates a metrics handler for tinytcp.Server. It can be registered using OnMetricsUpdate method.
 // Created handler exposes all server metrics to the given prometheus.Registerer.
-func NewHandler(registerer prometheus.Registerer) func(metrics tinytcp.ServerMetrics) {
+func NewHandler(
+	registerer prometheus.Registerer,
+	config ...*Config,
+) func(metrics tinytcp.ServerMetrics) {
+	c := &Config{}
+	if config != nil {
+		c = config[0]
+	}
+
 	totalRead := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "total_read",
-		Help: "Total number of bytes read by the server.",
+		Name:      "total_read",
+		Help:      "Total number of bytes read by the server.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 	totalWritten := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "total_written",
-		Help: "Total number of bytes written by the server.",
+		Name:      "total_written",
+		Help:      "Total number of bytes written by the server.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 	readLastSecond := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "read_last_second",
-		Help: "Total number of bytes read by the server last second.",
+		Name:      "read_last_second",
+		Help:      "Total number of bytes read by the server last second.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 	writtenLastSecond := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "written_last_second",
-		Help: "Total number of bytes written by the server last second.",
+		Name:      "written_last_second",
+		Help:      "Total number of bytes written by the server last second.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 	connections := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "connections",
-		Help: "Total number of active connections during the last second.",
+		Name:      "connections",
+		Help:      "Total number of active connections during the last second.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 	goroutines := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "goroutines",
-		Help: "Total number of active goroutines during the last second.",
+		Name:      "goroutines",
+		Help:      "Total number of active goroutines during the last second.",
+		Namespace: c.Namespace,
+		Subsystem: c.Subsystem,
 	})
 
 	registerer.MustRegister(
